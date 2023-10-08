92d0245a043817fd72fe882d94f14088bbc837c3
payt0nc <payton@nanami.me>
2023-10-08 16:58:29 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "hpchan-${prefix}-terraform-lab-bucket"
+  bucket = "hpchan-${var.prefix}-terraform-lab-bucket"
   acl    = "private"
 
   control_object_ownership = true
